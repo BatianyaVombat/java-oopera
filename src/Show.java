e7380aee585c726721a2c5bf58f2333a5f8934c2
BatianyaVombat <alex.strange.game@yandex.ru>
2026-03-22 13:40:25 +0300

    Добавил модификаторы доступа к переменным

--- src/Show.java
+++ src/Show.java
@@ -1,10 +1,10 @@
 import java.util.ArrayList;
 
 public class Show {
-    String title;
-    int duration;
-    Director director;
-    ArrayList<Actor> listOfActors;
+    private final String title;
+    protected final int duration;
+    protected Director director;
+    protected ArrayList<Actor> listOfActors;
 
     public Show(String title, int duration, Director director, ArrayList<Actor> listOfActors) {
         this.title = title;
